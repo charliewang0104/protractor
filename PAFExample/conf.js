4e30a4a519b071818baf285e1ea16488476fe51a
Charlie Wang <charliewang0104@gmail.com>
2016-10-20 21:56:37 -0400

    Add jasmine reporter support

--- PAFExample/conf.js
+++ PAFExample/conf.js
@@ -7,5 +7,26 @@ exports.config = {
   },
   capabilities:{
     browserName: 'chrome'
+  },
+  onPrepare: function() {
+	var jasmineReporters = require('jasmine-reporters');
+	
+	// returning the promise makes protractor wait for the reporter config before executing tests
+    return browser.getProcessedConfig().then(function(config) {
+        // you could use other properties here if you want, such as platform and version
+        var browserName = config.capabilities.browserName;
+
+        var junitReporter = new jasmineReporters.JUnitXmlReporter({
+            consolidateAll: false,
+            savePath: 'TestResults',
+            modifyReportFileName: function(generatedFileName, suite) {
+                // this will produce distinct file names for each capability,
+                // e.g. 'firefox.SuiteName' and 'chrome.SuiteName'
+                return browserName + '.' + generatedFileName ;
+            }
+        });
+        jasmine.getEnv().addReporter(junitReporter);
+    });
+//	jasmine.getEnv().addReporter(new jasmineReporters.TerminalReporter());
   }
 };
